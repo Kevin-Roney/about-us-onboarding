b4cd43ceeb6ba8c5d152f23158b1c3b169ab3223
JeffAllison <itsjallison@gmail.com>
2022-03-02 14:50:20 -0800

    created data.js, added export const info for jeff

---
+++ data.js
@@ -0,0 +1,15 @@
+export const people = [
+    {
+        id: 1,
+        name: 'Jeff',
+        pronouns: 'he/him',
+        favorite_place: 'warm beach',
+        hobbies: ['music', 'cooking', 'hiking'],
+    },
+    {
+        id: 2,
+        name: 'Dorothy',
+        pronouns: 'she/her',
+        favorite_place: 'couch',
+        hobbies: ['knitting', 'sewing', 'coding'],
+    }
