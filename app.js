3c0625c93dba8ac42d6b8cf1c46e2821f207ed25
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-02 15:24:54 -0800

    added DOM elements to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // import functions
-
+import { people } from './data.js';
 // grab DOM elements
-
+const selectElement = document.querySelector('.select');
+const divContainer = document.querySelector('.contain');
+const name = document.getElementById('name');
+const pronouns = document.querySelector('.pronouns');
+const favPlace = document.querySelector('.fav-place');
+const hobbies = document.querySelector('.hobbies');
 // set event listeners 
     // get user input
     // use user input to update state 
